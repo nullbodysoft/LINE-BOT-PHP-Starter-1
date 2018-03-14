4d1a735a23dcacae4ca15458dc7e9a76501ebb56
nullbodysoft <nullbodysoft@gmail.com>
2018-03-14 14:44:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,14 +11,15 @@ if (!is_null($events['events'])) {
     // Reply only when message sent is in 'text' format		
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			
       // Get text sent			
-      $text = $event['message']['text'];			
+      $text = $event['message']['text'];	
+      if($text=="summary")
+        $text ="สรุปการเปิด Logbook IDC";
       // Get replyToken			
       $replyToken = $event['replyToken'];			
       // Build message to reply back			
       $messages = ['type' => 'text','text' => $text];			
       // Make a POST Request to Messaging API to reply to sender
-      if($messages=="summary")
-        $messages="สรุปการเปิด Logbook IDC";
+      
       $url = 'https://api.line.me/v2/bot/message/reply';			
       $data = ['replyToken' => $replyToken,'messages' => [$messages],];			
       $post = json_encode($data);			
